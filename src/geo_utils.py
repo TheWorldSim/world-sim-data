2e6659b0b6c6a2002218fd44966e31a861e59f7f
Alexander James Phillips <ajp.global@gmail.com>
2026-06-07 23:22:30 +0100

    Add geo_utils

---
+++ src/geo_utils.py
@@ -0,0 +1,36 @@
+from shapely import LineString, Polygon
+
+
+def safe_join_linestrings(linestrings: list[LineString]) -> LineString:
+    last_point = linestrings[0].coords[0]
+    coords = [last_point]
+    for line in linestrings:
+        point1 = line.coords[0]
+        points = line.coords[1:]
+        if point1 == last_point:
+            coords.extend(points)
+            last_point = points[-1]
+        else:
+            raise ValueError(f"Line {line} does not connect to the previous line at point {last_point}")
+    return LineString(coords)
+
+
+def join_linestrings(linestrings: list[LineString], close=False) -> LineString:
+    last_point = linestrings[0].coords[0]
+    coords = [last_point]
+    for line in linestrings:
+        point1 = line.coords[0]
+        points = line.coords[1:]
+        if point1 == last_point:
+            coords.extend(points)
+            last_point = points[-1]
+        else:
+            print(f"Warning: Line {line} does not connect to the previous line at point {last_point}. Joining anyway.")
+            coords.append(point1)
+            coords.extend(points)
+            last_point = points[-1]
+
+    if close:
+        coords.append(coords[0])
+
+    return LineString(coords)
